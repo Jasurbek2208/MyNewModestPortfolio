b901e7b84a371fef3e199fc3d3530321ad4b0c86
Jasur <jasurbekshomaqsudov2004@gmail.com>
2023-07-18 18:49:04 +0300

    fix bags

--- src/service/axios/index.js
+++ src/service/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://shomaqsudov-portfolio-backend.glitch.me/";
+const BASE_URL = "https://portfolio-backend-livid-nine.vercel.app/";
 
 export const myAxios = axios.create({
     baseURL: BASE_URL,
