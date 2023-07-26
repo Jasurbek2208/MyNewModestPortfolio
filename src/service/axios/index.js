c82bb5e85212682f7ed49586ca48c56a34ee5f6f
Jasur <jasurbekshomaqsudov2004@gmail.com>
2023-07-26 17:23:01 +0300

    renamed base url

--- src/service/axios/index.js
+++ src/service/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://portfolio-backend-livid-nine.vercel.app/";
+const BASE_URL = "https://shomaqsudov-portfolio-backend.glitch.me/";
 
 export const myAxios = axios.create({
     baseURL: BASE_URL,
